fddf7c943b9134505882265592d85b767b4dfcd4
long-shaun <long_shaun@163.com>
2019-02-13 20:36:58 +0800

    feat:商品列表

--- pages/mall/mall.js
+++ pages/mall/mall.js
@@ -1,11 +1,46 @@
 // pages/mall/mall.js
+import ajax from '../../utils/ajax.js'
 Page({
+
+  lower(e){
+    if(!(this.data.isEnd)){
+      this.getList(true)
+    }
+  },
+  // 上面tab点击事件
+  handleChange(e) {
+    this.setData({
+      actionId: this.data.tab[e.detail.index].id,
+      scrollTop: 0,
+      start: 0
+    })
+    this.getList()
+  },
+
+  // 判断加载什么类容，对数组的进行操作
+  getList(isLoadMore = false) {
+    const { actionId, start } = this.data
+    ajax.getData(`http://www.xiongmaoyouxuan.com/api/tab/${actionId}?start=${start}`)
+      .then(res => {
+        const list = isLoadMore ? this.data.list.concat(res.items.list) : res.items.list
+        this.setData({
+          list,
+          isEnd: res.items.isEnd,
+          start: res.items.nextIndex
+        })
+      })
+  },
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tab: [],
+    list: [],
+    actionId: 0,
+    start: 0,
+    isEnd: false,
+    scrollTop: 0
   },
 
   /**
@@ -19,7 +54,15 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    ajax.getData('http://www.xiongmaoyouxuan.com/api/tabs?sa=')
+      .then(res => {
+        this.setData({
+          tab: res.list,
+          actionId: res.list[0].id
+        }, () => {
+          this.getList()
+        })
+      })
   },
 
   /**
