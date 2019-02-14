e8f8d69043c656bd372a97e6e7f0927faf8f563e
Long <342826844@qq.com>
2019-02-15 00:16:56 +0800

    feat:购物车完成

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -3,12 +3,92 @@ import ajax from '../../utils/ajax.js'
 const app = getApp()
 Page({
   handleAllCheck(){
-    this.setData({
-      allChecked: !(this.data.allChecked)
+    const cartList = this.data.cartList.map(item => {
+      item.isChecked = !(this.data.allChecked)
+      return item
     })
+    this.setData({
+      cartList
+    })
+    this.totalAllCheck()
+    this.totalCheckedPrice()
+  },
+  totalAllCheck(){
+    const allChecked =  this.data.cartList.every(item => (item.isChecked === true))
+    console.log(this.data.cartList)
+    this.setData({
+      allChecked
+    })
+  },
+  totalCheckedPrice(){
+    const checkedPrice =  this.data.cartList.reduce((result, item) => {
+      if(item.isChecked){
+        result += item.price * item.count*100
+      }
+      return result
+    },0)
+    this.setData({
+      checkedPrice
+    })
+  },
+  handleCheck(e){
+    console.log(e)
+    this.totalCount(e, "Check")
+    this.totalAllCheck()
+  },
+  handleMinusCount(e){
+    this.totalCount(e,"minus")
+  },
+  handleAddCount(e){
+    this.totalCount(e, "add")
+  },
+  totalCount(e,type){
+    const { id } = e.target.dataset
+    const cartList = this.data.cartList.map(item => {
+      if(item.id === id){
+        switch(type){
+          case "add":
+            this.setCartStorage(id,1)
+            item.count += 1
+            return item
+          case "minus":
+            if (item.count === 1){
+              return item
+            }
+            this.setCartStorage(id, -1)
+            item.count -= 1
+            return item
+          case "Check":
+            item.isChecked = !item.isChecked
+            return item
+
+          default:
+            return item
+        }
+      } 
+      return item
+    })
+    this.setData({
+      cartList
+    })
+    this.totalCheckedPrice()
+  },
+  setCartStorage(id,temp){
+    let storage = wx.getStorageSync("lxa_cart");
+    let carts = storage ? JSON.parse(storage) : []
+    const cart = carts.map(item => {
+      if(item.id === id){
+        item.count += temp
+        return item
+      }
+      return item
+    })
+    wx.setStorageSync("lxa_cart", JSON.stringify(cart))
+    app.getCartCount()
   },
   carts: [],
   getCardList(){
+    console.log("gitCard")
     const requests = this.carts.map(item => {
       return ajax.getData(`http://www.xiongmaoyouxuan.com/api/detail?id=${item.id}&normal=1&sa=`)
     })
@@ -22,14 +102,15 @@ Page({
           return item
         })
         console.log(detail)
-        detail.map((item,index) =>{
+        const cartLists = detail.map((item,index) =>{
           const {
             id,
             accessoryHints,
             title,
             price,
             originPrice,
-            qunTitle
+            qunTitle,
+            image
           } = item
           return {
             id,
@@ -38,9 +119,21 @@ Page({
             price,
             originPrice,
             qunTitle,
-            count: this.cart[index].count
+            image,
+            count: this.carts[index].count,
+            isChecked: false
           }
         })
+        return cartLists
+      })
+      .then(res => {
+        console.log(res)
+        this.setData({
+          cartList: res
+        })
+      })
+      .then(() => {
+        this.totalAllCheck()
       })
   },
   /**
@@ -49,7 +142,8 @@ Page({
   data: {
     imageURL: '//img.yzcdn.cn/upload_files/2017/07/02/af5b9f44deaeb68000d7e4a711160c53.jpg',
     cart: [1,2,3,4,5],
-    allChecked: true,
+    allChecked: false,
+    checkedPrice: 0,
     cartList: []
   },
 
